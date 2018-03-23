47ed776b80d6e63bb79ad783554e9ef9f3bb58c7
zhanghanduo <hzhang032@e.ntu.edu.sg>
2018-03-23 11:51:51 +0800

    :bookmark: initial commit, add stereo node and launch files

--- src/publishCameraInfo.cpp
+++ src/publishCameraInfo.cpp
@@ -1,14 +1,12 @@
-//contains cameraInfo for snapcam.
-//MUST RENAME NODE:  snap_cam_highres_publisher/image (etc) to image_raw
-//MUST SET CAMFILE:  rosparam set camfile snapcam1 (etc)
-#include "ros/ros.h"
+//contains cameraInfo for mono camera.
+
+#include <ros/ros.h>
+#include <ros/package.h>
 #include "sensor_msgs/CameraInfo.h"
 #include "sensor_msgs/CompressedImage.h"
 #include "sensor_msgs/Image.h"
 #include "camera_info_manager/camera_info_manager.h"
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+
 using namespace std;
 
 /**Code adapted from
@@ -19,52 +17,55 @@ http://answers.ros.org/question/53234/processing-an-image-outside-the-callback-f
 class publishCameraInfo
 {
 public:
-  publishCameraInfo(const ros::NodeHandle &nh, const ros::NodeHandle n): nh_(nh), n_(n)
+  publishCameraInfo(const ros::NodeHandle &nh, const ros::NodeHandle &n): nh_(nh), n_(n)
   {
-      if(n_.getParam("topic/left", left_topic))
-          ROS_INFO("Get left image info topic: %s", left_topic.c_str());
+      string img_topic = "/camera_raw";
+
+      if(n_.getParam("topic/img", img_topic))
+          ROS_INFO("Get image info topic: %s", img_topic.c_str());
       else
-          ROS_WARN("Use default left image info topic: %s", left_topic.c_str());
-
-      if(n_.getParam("topic/right", right_topic))
-          ROS_INFO("Get right image info topic: %s", right_topic.c_str());
+          ROS_WARN("Use default image info topic: %s", img_topic.c_str());
+      if(n_.getParam("topic/info", info_topic))
+          ROS_INFO("Get image info topic: %s", info_topic.c_str());
       else
-          ROS_WARN("Use default right image info topic: %s", right_topic.c_str())
+          ROS_WARN("Use default image info topic: %s", info_topic.c_str());
 
-      pub_left_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(left_topic, 1);
-      pub_right_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(right_topic, 1);
+      pub_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(info_topic, 1);
 
-//      sub_left = nh_.subscribe("image_raw", 1, &publishCameraInfo::callback, this);
+      sub_img = nh_.subscribe(img_topic, 1, &publishCameraInfo::callback, this);
   }
-  void callback(const sensor_msgs::Image& imgmsg1, const sensor_msgs::Image& imgmsg2)
+  void callback(const sensor_msgs::ImageConstPtr& imgmsg)
   {
-    //not matching camname to camera name in input file throws an error in camera_info_manager.
-    //This error can be ignored.
-//    const std::string camurl="file:///home/rnl/.ros/camera_info/snapcam1.yaml";
+    //  not matching camname to camera name in input file throws an error in camera_info_manager.
+    //  This error can be ignored.
+    //  const std::string camurl="file:///home/rnl/.ros/camera_info/snapcam1.yaml";
     std::string camname;
-    n_.getParam ( "camfile", camname);  //camname is set by:  rosparam set camfile _____
-    const std::string camnameConst=camname;
-    const std::string camurlRead="file:///home/rnl/.ros/camera_info/" + camname + ".yaml";
-    camera_info_manager::CameraInfoManager caminfo(n_, camnameConst,camurlRead);
-//    sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(caminfo->getCameraInfo()));  //copied
-//    camera_info_manager::CameraInfoManager caminfo(n_, camname);
-    sensor_msgs::CameraInfo ci;
-    ci=caminfo.getCameraInfo();
 
-    ci.header.stamp = imgmsg1.header.stamp;
-    ci.header.frame_id = imgmsg1.header.frame_id;
+    n_.getParam ( "camfile", camname);  //camname is set by:  rosparam set camfile _____
+
+    const std::string camnameConst = camname;
+
+    const std::string camurlRead = "file://" + ros::package::getPath("undistort_images") + "/calib_files/" + camname + ".yaml";
+
+    camera_info_manager::CameraInfoManager caminfo(nh_, camnameConst, camurlRead);
+
+    sensor_msgs::CameraInfo ci;
+
+    ci = caminfo.getCameraInfo();
+
+    ci.header.stamp = imgmsg->header.stamp;
+    ci.header.frame_id = imgmsg->header.frame_id;
     
     // Publish via image_transport
-      pub_left_cam_info.publish(ci);
+    pub_cam_info.publish(ci);
   }
 
   private:
   ros::NodeHandle n_,nh_;
-  ros::Publisher pub_left_cam_info, pub_right_cam_info;
-  ros::Subscriber sub_left, sub_right;
+  ros::Publisher pub_cam_info;
+  ros::Subscriber sub_img;
 
-  string left_topic = "/wide/left/camera_info";
-  string right_topic = "/wide/right/camera_info";
+  string info_topic = "/camera_info";
 
 };//End of class
 
@@ -76,17 +77,9 @@ int main(int argc, char **argv)
 
     publishCameraInfo cameraPubObject(nh, n);
 
-    message_filters::Subscriber<sensor_msgs::Image> image1_sub(nh, "/wide/left/image_raw", 1);
-    message_filters::Subscriber<sensor_msgs::Image> image2_sub(nh, "/wide/right/image_raw", 1);
-
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
-    // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), image1_sub, image2_sub);
-    sync.registerCallback(boost::bind(&publishCameraInfo::callback, &cameraPubObject ,_1, _2));
-
     ros::spin();
 
-  return 0;
+    return 0;
 }
 
 
---
+++ src/publishStereoCameraInfo.cpp
@@ -0,0 +1,124 @@
+//contains cameraInfo for stereo cameras with rec and proj matrix.
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/CompressedImage.h"
+#include "sensor_msgs/Image.h"
+#include "camera_info_manager/camera_info_manager.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+using namespace std;
+
+/**Code adapted from
+https://github.com/nickMont/cameraInfoPublisher
+*/
+class publishCameraInfo
+{
+public:
+  publishCameraInfo(const ros::NodeHandle &nh, const ros::NodeHandle &n): nh_(nh), n_(n)
+  {
+      if(n_.getParam("topic/left_info", left_info_topic))
+          ROS_INFO("Get left image info topic: %s", left_info_topic.c_str());
+      else
+          ROS_WARN("Use default left image info topic: %s", left_info_topic.c_str());
+
+      if(n_.getParam("topic/right_info", right_info_topic))
+          ROS_INFO("Get right image info topic: %s", right_info_topic.c_str());
+      else
+          ROS_WARN("Use default right image info topic: %s", right_info_topic.c_str());
+
+      pub_left_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(left_info_topic, 1);
+      pub_right_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(right_info_topic, 1);
+
+//      sub_left = nh_.subscribe("image_raw", 1, &publishCameraInfo::callback, this);
+  }
+  void callback(const sensor_msgs::ImageConstPtr& imgmsg1, const sensor_msgs::ImageConstPtr& imgmsg2)
+  {
+    //  not matching camname to camera name in input file throws an error in camera_info_manager.
+    //  This error can be ignored.
+    //  const std::string camurl="file:///home/rnl/.ros/camera_info/snapcam1.yaml";
+    std::string camname_left, camname_right;
+
+    n_.getParam ( "camfile_left", camname_left);  //camname is set by:  rosparam set camfile _____
+
+    n_.getParam ( "camfile_right", camname_right);
+
+    const std::string camnameConst_l = camname_left;
+
+    const std::string camnameConst_r = camname_right;
+
+    const std::string camurlRead_l = "file://" + ros::package::getPath("undistort_images") + "/calib_files/" + camname_left + ".yaml";
+
+    const std::string camurlRead_r = "file://" + ros::package::getPath("undistort_images") + "/calib_files/" + camname_right + ".yaml";
+
+    camera_info_manager::CameraInfoManager caminfo_l(nh_, camnameConst_l, camurlRead_l);
+
+    camera_info_manager::CameraInfoManager caminfo_r(nh_, camnameConst_r, camurlRead_r);
+
+//    sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(caminfo->getCameraInfo()));  //copied
+//    camera_info_manager::CameraInfoManager caminfo(n_, camname);
+    sensor_msgs::CameraInfo ci_left, ci_right;
+
+    ci_left = caminfo_l.getCameraInfo();
+
+    ci_left.header.stamp = imgmsg1->header.stamp;
+    ci_left.header.frame_id = imgmsg1->header.frame_id;
+
+    ci_right = caminfo_r.getCameraInfo();
+
+    ci_right.header.stamp = imgmsg2->header.stamp;
+    ci_right.header.frame_id = imgmsg2->header.frame_id;
+    
+    // Publish via image_transport
+    pub_left_cam_info.publish(ci_left);
+    pub_right_cam_info.publish(ci_right);
+  }
+
+  private:
+  ros::NodeHandle n_,nh_;
+  ros::Publisher pub_left_cam_info, pub_right_cam_info;
+//  ros::Subscriber sub_left, sub_right;
+
+  string left_info_topic = "/wide/left/camera_info";
+  string right_info_topic = "/wide/right/camera_info";
+
+};//End of class
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "publishStereoCameraInfo");
+    ros::NodeHandle nh;
+    ros::NodeHandle n("~");
+
+    string left_img_topic = "/wide/left/camera_raw";
+    string right_img_topic = "/wide/right/camera_raw";
+
+    if(n.getParam("topic/left_img", left_img_topic))
+        ROS_INFO("Get left image info topic: %s", left_img_topic.c_str());
+    else
+        ROS_WARN("Use default left image info topic: %s", left_img_topic.c_str());
+
+    if(n.getParam("topic/right_img", right_img_topic))
+        ROS_INFO("Get right image info topic: %s", right_img_topic.c_str());
+    else
+        ROS_WARN("Use default right image info topic: %s", right_img_topic.c_str());
+
+    publishCameraInfo cameraPubStereoObject(nh, n);
+
+    message_filters::Subscriber<sensor_msgs::Image> image1_sub(nh, left_img_topic, 5);
+    message_filters::Subscriber<sensor_msgs::Image> image2_sub(nh, right_img_topic, 5);
+
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+    // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), image1_sub, image2_sub);
+    sync.registerCallback(boost::bind(&publishCameraInfo::callback, &cameraPubStereoObject ,_1, _2));
+
+    ros::spin();
+
+    return 0;
+}
+
+
+
