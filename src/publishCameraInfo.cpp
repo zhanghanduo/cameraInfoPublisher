04d1347bc61bc60d65060b2a540489f2d58c8073
handuo <zhanghanduo@live.cn>
2018-05-24 15:32:05 +0800

    :sparkles: add crop camera info function

--- src/publishCameraInfo.cpp
+++ src/publishCameraInfo.cpp
@@ -32,7 +32,28 @@ public:
       else
           ROS_WARN("Use default image info topic: %s", info_topic.c_str());
 
+      if(n.getParam("cut", cut_))
+          ROS_INFO("Get cut flag: %s", cut_? "true":"false");
+      else
+          ROS_WARN("Using default cut flag: false!");
+
+      if(n.getParam("x_offset", x_off))
+          ROS_INFO("Get x offset: %u", x_off);
+
+      if(n.getParam("y_offset", y_off))
+          ROS_INFO("Get x offset: %u", y_off);
+
+      if(n.getParam("width", w_off))
+          ROS_INFO("Get width offset: %u", w_off);
+
+      if(n.getParam("height", h_off))
+          ROS_INFO("Get height offset: %u", h_off);
+
+
       pub_cam_info = nh_.advertise<sensor_msgs::CameraInfo>(camname + info_topic, 1);
+
+      if(cut_)
+          pub_cam_info_cut = nh_.advertise<sensor_msgs::CameraInfo>(camname + "_crop" + info_topic, 1);
 
       sub_img = nh_.subscribe(img_topic, 1, &publishCameraInfo::callback, this);
   }
@@ -45,22 +66,37 @@ public:
 
     camera_info_manager::CameraInfoManager caminfo(nh_, camnameConst, camurlRead);
 
-    sensor_msgs::CameraInfo ci;
+    sensor_msgs::CameraInfo ci, ci_cut;
 
     ci = caminfo.getCameraInfo();
 
     ci.header.stamp = imgmsg->header.stamp;
     ci.header.frame_id = imgmsg->header.frame_id;
 
+    ci_cut = ci;
+
+    ci_cut.binning_x = ci_cut.binning_y = 1;
+    ci_cut.roi.height = h_off;
+    ci_cut.roi.width = w_off;
+    ci_cut.roi.x_offset = x_off;
+    ci_cut.roi.y_offset = y_off;
+    ci_cut.roi.do_rectify = cut_;
 
     // Publish via image_transport
     pub_cam_info.publish(ci);
+    if(cut_)
+        pub_cam_info_cut.publish(ci_cut);
   }
 
   private:
   ros::NodeHandle n_,nh_;
   ros::Publisher pub_cam_info;
+  ros::Publisher pub_cam_info_cut;
   ros::Subscriber sub_img;
+
+  bool cut_;
+
+  int x_off, y_off, w_off, h_off;
 
   string info_topic, camname;
 
